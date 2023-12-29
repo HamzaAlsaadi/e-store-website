828f926a9a4bedc91c23812bda4de86b07bcde0f
HamzaAlsaadi <70109851+HamzaAlsaadi@users.noreply.github.com>
2023-12-29 21:41:27 +0200

    Update api.php

--- e-store/routes/api.php
+++ e-store/routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('products', ProudctContoller::class);
+Route::apiResource('products', ProductController::class);
 
