a8e0354f5e4798d70c30a568c077fdd47b25bb3e
HamzaAlsaadi <70109851+HamzaAlsaadi@users.noreply.github.com>
2024-01-02 11:52:34 +0200

    add api
    add api for product rating

--- e-store/app/Models/Product.php
+++ e-store/app/Models/Product.php
@@ -8,21 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $table = 'products';
     protected $fillable = [
-        'mobile_name', 'Cpu_spsecfication', 'Gpu_spsecfication', 'battery_spsecfication', 'Front_camera_spsecfication', 'Back_camera_spsecfication', 'Screen_Size', 'Type_of_charge', 'Price', 'imge', 'Company_id', 'category_id'
+        'id', 'mobile_name', 'Cpu_spsecfication', 'Gpu_spsecfication', 'battery_spsecfication', 'Front_camera_spsecfication', 'Back_camera_spsecfication', 'Screen_Size', 'Type_of_charge', 'Price', 'imge', 'Company_id', 'category_id'
     ];
 
     public function company()
     {
-        return $this->belongsTo(Company::class,'Company_id');
+        return $this->belongsTo(Company::class, 'Company_id');
     }
 
     public function category()
     {
-        return $this->belongsTo(Category::class,'category_id');
+        return $this->belongsTo(Category::class, 'category_id');
     }
 
-    public function rate()
+    public function ratings()
     {
         return $this->hasMany(RateProduct::class);
     }
--- e-store/routes/api.php
+++ e-store/routes/api.php
@@ -2,11 +2,15 @@
 
 use App\Http\Controllers\Api\AcessController;
 use App\Http\Controllers\Api\CatgoryContoller;
+use App\Http\Controllers\Api\CodeCheckController;
 use App\Http\Controllers\Api\CompanyController;
+use App\Http\Controllers\Api\ForgotPasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\ProductRatingController;
+use App\Http\Controllers\Api\ResetPasswordController;
 use App\Http\Controllers\Api\StoreCsvController;
 
 /*
@@ -24,10 +28,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('products', ProductController::class);
+Route::apiResource('products', ProductController::class)->middleware('auth:sanctum');;
 Route::apiResource('catgory', CatgoryContoller::class);
 Route::apiResource('Company', CompanyController::class);
-Route::apiResource('csv', StoreCsvController::class);
-Route::apiResource('User', UserController::class);
+Route::apiResource('csv', StoreCsvController::class)->middleware('auth:sanctum');;
+Route::apiResource('User', UserController::class)->middleware('auth:sanctum');;
 Route::post('/auth/register', [UserController::class, 'createUser']);
 Route::post('/auth/login', [UserController::class, 'loginUser']);
+
+Route::post('products/{productId}/rate', [ProductRatingController::class, 'rateProduct'])->middleware('auth:sanctum');
+
+
+
+
+Route::post('password/email',  ForgotPasswordController::class);
+Route::post('password/code/check', CodeCheckController::class);
+Route::post('password/reset', ResetPasswordController::class);
