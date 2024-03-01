f9be6340027d68e6737f001e50b5358c9a118a16
HamzaAlsaadi <70109851+HamzaAlsaadi@users.noreply.github.com>
2024-03-01 13:03:53 +0200

    s
    s

--- e-store/app/Http/Controllers/Api/CompanyController.php
+++ e-store/app/Http/Controllers/Api/CompanyController.php
@@ -36,7 +36,7 @@ class CompanyController extends Controller
             $filename = time() . '.' . $image->getClientOriginalExtension();
             $image->move($path, $filename);
 
-            $validatedData['imge'] =  $filename;
+            $validatedData['image'] =  $filename;
         }
         // var_dump($validatedData);
         $company = Company::create($validatedData);
@@ -78,7 +78,7 @@ class CompanyController extends Controller
             $filename = time() . '.' . $image->getClientOriginalExtension();
             $image->move($path, $filename);
 
-            $validatedData['imge'] =  $filename;
+            $validatedData['image'] =  $filename;
         }
         $company->update($validatedData);
         return response()->json($company, 200);
